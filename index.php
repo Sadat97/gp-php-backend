c2865fc3ae31d3b3e2b67765a8607cc175c32b5a
Mohamed Anwar <moanwar1997@gmail.com>
2019-06-15 22:36:40 +0200

    fix edge case errors

--- index.php
+++ index.php
@@ -5,13 +5,15 @@ require('./vendor/autoload.php');
 $slim = new Slim\App;
 
 $slim->post('/calc', $download = function ($request, $response) {
+    $keys = $request->getParsedBody();
     $target = $request->getParsedBody()['target'];
     $arr_size = $request->getParsedBody()['array_size'];
     $numbers = $request->getParsedBody()['numbers'];
    
-    if (!$target || !$numbers || !$arr_size) {
+    // if ( !$target || !$numbers || !$arr_size) {
+    if (!array_key_exists("target", $keys) || !array_key_exists("array_size", $keys) || !array_key_exists("numbers", $keys)){   
         $data = array('error' => 'Please send all the required fields');
-        $newResponse = $response->withJson($data, 402);
+        $newResponse = $response->withJson($data, 401);
     } else {
         $data = array('result' => getResult($target, $arr_size, $numbers));
         $newResponse = $response->withJson($data, 201);
